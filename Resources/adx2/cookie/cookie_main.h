0a9796a8a91150aa39147101aeac049bc7c68bd7
Tetsu Yamashita <tetu_fs@live.jp>
2014-07-17 14:12:37 +0900

    なんとなくメインBGM作成

---
+++ Resources/adx2/cookie/cookie_main.h
@@ -0,0 +1,28 @@
+/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : cookie_crush
+ *  Tool Ver.        : CRI Atom Craft LE Ver.1.32.00
+ *  File Path        : F:/music tools/cri/projects/cookie/Public/cookie_main.acb
+ *  File Name        : cookie_main.acb
+ *  File Size        : 4,340,064 bytes
+ *  Date Time        : 2014/07/17 14:03
+ *  Target           : Public
+ *  Cues             : 3
+ *  CueSheet Comment : 
+ *===========================================================================*/
+
+#define CRI_COOKIE_MAIN_CUENUM (3)
+
+/* AISAC Control List (AISAC Control ID) */
+// No AISAC Control
+
+
+/* Cue List (Cue ID) */
+#define CRI_COOKIE_MAIN_VANISH  ( 0) /*   */
+#define CRI_COOKIE_MAIN_SWIPE   ( 1) /*   */
+#define CRI_COOKIE_MAIN_BGM     ( 2) /*   */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
