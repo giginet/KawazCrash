59299be46510dcca6d0baa1ddda3452001fab6f1
Tetsu Yamashita <tetu_fs@live.jp>
2014-08-06 20:46:45 +0900

    開始と終了の効果音を追加

--- Classes/cookie_main.h
+++ Classes/cookie_main.h
@@ -4,14 +4,14 @@
  *  Tool Ver.        : CRI Atom Craft LE Ver.1.32.00
  *  File Path        : F:/music tools/cri/projects/cookie/Public/cookie_main.acb
  *  File Name        : cookie_main.acb
- *  File Size        : 4,340,160 bytes
- *  Date Time        : 2014/07/18 20:21
+ *  File Size        : 4,439,776 bytes
+ *  Date Time        : 2014/08/06 20:22
  *  Target           : Public
- *  Cues             : 3
+ *  Cues             : 5
  *  CueSheet Comment : 
  *===========================================================================*/
 
-#define CRI_COOKIE_MAIN_CUENUM (3)
+#define CRI_COOKIE_MAIN_CUENUM (5)
 
 /* AISAC Control List (AISAC Control ID) */
 // No AISAC Control
@@ -21,6 +21,8 @@
 #define CRI_COOKIE_MAIN_VANISH  ( 0) /*   */
 #define CRI_COOKIE_MAIN_SWIPE   ( 1) /*   */
 #define CRI_COOKIE_MAIN_BGM     ( 2) /*   */
+#define CRI_COOKIE_MAIN_START   ( 3) /*   */
+#define CRI_COOKIE_MAIN_END     ( 4) /*   */
 
 /* Block List (Block Index) */
 
