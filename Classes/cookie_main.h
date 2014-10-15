3369fe644d6c17f751cb53dec92ff6177eb77e07
Tetsu Yamashita <tetu_fs@live.jp>
2014-10-15 21:03:30 +0900

    タイトルとゲーム終了時の決定音2種追加

--- Classes/cookie_main.h
+++ Classes/cookie_main.h
@@ -4,14 +4,14 @@
  *  Tool Ver.        : CRI Atom Craft LE Ver.1.32.00
  *  File Path        : F:/music tools/cri/projects/cookie/Public/cookie_main.acb
  *  File Name        : cookie_main.acb
- *  File Size        : 4,439,776 bytes
- *  Date Time        : 2014/09/10 22:57
+ *  File Size        : 4,488,960 bytes
+ *  Date Time        : 2014/10/15 20:52
  *  Target           : Public
- *  Cues             : 5
+ *  Cues             : 6
  *  CueSheet Comment : 
  *===========================================================================*/
 
-#define CRI_COOKIE_MAIN_CUENUM (5)
+#define CRI_COOKIE_MAIN_CUENUM (6)
 
 /* AISAC Control List (AISAC Control ID) */
 // No AISAC Control
@@ -23,6 +23,7 @@
 #define CRI_COOKIE_MAIN_BGM     ( 2) /*   */
 #define CRI_COOKIE_MAIN_START   ( 3) /*   */
 #define CRI_COOKIE_MAIN_END     ( 4) /*   */
+#define CRI_COOKIE_MAIN_RETURN  ( 5) /*   */
 
 /* Block List (Block Index) */
 
--- Classes/cookie_title.h
+++ Classes/cookie_title.h
@@ -4,21 +4,22 @@
  *  Tool Ver.        : CRI Atom Craft LE Ver.1.32.00
  *  File Path        : F:/music tools/cri/projects/cookie/Public/cookie_title.acb
  *  File Name        : cookie_title.acb
- *  File Size        : 793,216 bytes
- *  Date Time        : 2014/09/10 22:57
+ *  File Size        : 843,488 bytes
+ *  Date Time        : 2014/10/15 20:52
  *  Target           : Public
- *  Cues             : 1
+ *  Cues             : 2
  *  CueSheet Comment : 
  *===========================================================================*/
 
-#define CRI_COOKIE_TITLE_CUENUM (1)
+#define CRI_COOKIE_TITLE_CUENUM (2)
 
 /* AISAC Control List (AISAC Control ID) */
 // No AISAC Control
 
 
 /* Cue List (Cue ID) */
-#define CRI_COOKIE_TITLE_TITLE  ( 5) /*   */
+#define CRI_COOKIE_TITLE_TITLE   ( 5) /*   */
+#define CRI_COOKIE_TITLE_CHOICE  ( 6) /*   */
 
 /* Block List (Block Index) */
 
